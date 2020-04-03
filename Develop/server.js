0fd05ff9c80e5180deefe4fd838afc325c47f4f4
Brandon Kim <bkim377@gatech.edu>
2020-04-02 20:21:57 -0400

    Checkpoint 2 - got the server properly working, started on POST functions

--- Develop/server.js
+++ Develop/server.js
@@ -1,37 +1,20 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// variables defining the reservations
-
-var reservations = [
-  { routeName: "john",
-    customerName: "John",
-    phoneNumber: "770-123-4567",
-    email: "john@gmail.com",
-    id: 2
-  },
-];
-var waitlist = [
-  { routeName: "john",
-    customerName: "John",
-    phoneNumber: "770-123-4567",
-    email: "john@gmail.com",
-    id: 1
-  },
-];
+// variables defining the notes
 
 // Route to serve index.html
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
-// Route to serve notes.html 
+// Route to serve notes.html
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
@@ -39,16 +22,16 @@ app.get("/notes", function(req, res) {
 // Route to serve JSON of the list of saved notes
 app.get("/api/notes", function(req, res) {
   fs.readFile("/db.json", function(err, data) {
-    const savedNotes = JSON.parse(data);
+    var savedNotes = JSON.parse(data);
     return res.json(savedNotes);
-  })
+  });
 });
 // Route to serve JSON of the list of wait-listed reservations
-app.get("/api/notes:id", function(req, res) {
-  const index = parseInt(req.params.id);
+app.get("/api/notes/:id", function(req, res) {
+  var index = parseInt(req.params.id);
   console.log(index);
   if (isNaN(index)) {
-      return res.send("Please enter a valid ID");
+    return res.send("Please enter a valid ID");
   }
 });
 
@@ -56,19 +39,15 @@ app.get("/api/notes:id", function(req, res) {
 app.post("/api/notes", function(req, res) {
   console.log(req.body);
   fs.readFile("/db.json", function(err, data) {
-
-  const notesArray = JSON.parse(data);
-  notesArray.push(req.body);
-  console.log(notesArray);
-  fs.writeFile("/db.json", JSON.stringify(notesArray), function(err) {
-
-    res.send("Note created!");
-  })
-
-
+    var notesArray = JSON.parse(data);
+    notesArray.push(req.body);
+    console.log(notesArray);
+    fs.writeFile("/db.json", JSON.stringify(notesArray), function(err) {
+      res.send("Note created!");
+    });
+  });
 });
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
-});
 });
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HW11-Note-Taker
 My submission for the HW11-Note-Taker application.
+
+1) Description: This is a note-taking application that allows the user to write, save, and delete notes on a single webpage.  It uses an express.js backend to save and retrieve note data from a JSON file.
+
+2) Link to deployed application: https://bkim377.github.io/HW11-Note-Taker
+
+3) Screenshot: ![NoteTaker Screenshot](Note Taker)

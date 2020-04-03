79e39a8c248936a67edef8c0bacd5b9564f18254
Brandon Kim <bkim377@gatech.edu>
2020-04-02 23:39:50 -0400

    Checkpoint 3 - got the api/notes route working

--- Develop/server.js
+++ Develop/server.js
@@ -8,7 +8,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// variables defining the notes
+// Routes
+// =============================================================
 
 // Route to serve index.html
 app.get("/", function(req, res) {
@@ -21,30 +22,18 @@ app.get("/notes", function(req, res) {
 
 // Route to serve JSON of the list of saved notes
 app.get("/api/notes", function(req, res) {
-  fs.readFile("/db.json", function(err, data) {
-    var savedNotes = JSON.parse(data);
-    return res.json(savedNotes);
-  });
-});
-// Route to serve JSON of the list of wait-listed reservations
-app.get("/api/notes/:id", function(req, res) {
-  var index = parseInt(req.params.id);
-  console.log(index);
-  if (isNaN(index)) {
-    return res.send("Please enter a valid ID");
-  }
+  res.sendFile(path.join(__dirname, "db.json"));
 });
 
-// Route to add new note
+// Route to add new note - takes in JSON input
 app.post("/api/notes", function(req, res) {
-  console.log(req.body);
-  fs.readFile("/db.json", function(err, data) {
+  var newNote = req.body;
+  fs.readFile("Develop/db.json", function(err, data) {
     var notesArray = JSON.parse(data);
-    notesArray.push(req.body);
+    notesArray.push(newNote);
     console.log(notesArray);
-    fs.writeFile("/db.json", JSON.stringify(notesArray), function(err) {
-      res.send("Note created!");
-    });
+    res.json(newNote);
+    res.send("Note created!");
   });
 });
 

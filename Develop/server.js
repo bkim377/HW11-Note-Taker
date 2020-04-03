0fdc52e751f6edd66d3cce53c32e8f3365d80a95
Brandon Kim <bkim377@gatech.edu>
2020-04-03 15:17:29 -0400

    Checkpoint 4 - got a lot of help from a TA

--- Develop/server.js
+++ Develop/server.js
@@ -1,23 +1,25 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // Routes
 // =============================================================
 
 // Route to serve index.html
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 // Route to serve notes.html
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 // Route to serve JSON of the list of saved notes
@@ -28,14 +30,37 @@ app.get("/api/notes", function(req, res) {
 // Route to add new note - takes in JSON input
 app.post("/api/notes", function(req, res) {
   var newNote = req.body;
-  fs.readFile("Develop/db.json", function(err, data) {
+  newNote.id = uuidv4();
+  fs.readFile("db.json", "utf8", function(err, data){
     var notesArray = JSON.parse(data);
     notesArray.push(newNote);
-    console.log(notesArray);
-    res.json(newNote);
-    res.send("Note created!");
-  });
+    var updatedArray = JSON.stringify(notesArray);
+    fs.writeFile("db.json", updatedArray, function(err) {
+        if (err) throw err;
+      });
+        res.send("Note created!");
+  })
+//   fs.appendFile("db.json", newNote, function(err) {
+//     if (err) throw err;
+//   });
+//     res.send("Note created!");
 });
+
+app.delete("/api/notes/:id", function(req, res) {
+    console.log(req.params.id);
+    fs.readFile("db.json", "utf8", function(err, data){
+    var notesArray = JSON.parse(data);
+    var newArray = notesArray.filter(note => {
+        console.log(note.id !== req.params.id);
+        return note.id !== req.params.id;
+    }); 
+    var updatedArray = JSON.stringify(newArray);
+    fs.writeFile("db.json", updatedArray, function(err) {
+        if (err) throw err;
+      });
+        res.send("Note created!");
+    })
+})
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
